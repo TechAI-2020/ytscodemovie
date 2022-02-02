205aa9ec70a96750b33aa999a7c80bb11107917e
TechAI-2020 <jonasestomih77@gmail.com>
2022-02-02 15:05:25 +0300

    list of decoration used

---
+++ lib/decorationspage/textdec.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+class Customtext extends StatelessWidget {
+  const Customtext({Key? key, required this.text, required this.color, required this.size}) : super(key: key);
+ final String text;
+ final Color color;
+ final double size;
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text ,style: GoogleFonts.breeSerif(
+      color: color,
+      fontSize: size,
+    ),
+
+    );
+  }
+}
